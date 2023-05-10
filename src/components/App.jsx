f606a7d00c8a4c5f4598ad4213450de830db77ac
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-10 23:47:11 +0300

    delete draft

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,11 @@
-import PageTitleSection from "./PageTitleSection/PageTitleSection";
-
 
 
 export const App = () => {
   return (
     <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101',
-      //   backgroundColor: "#ad8686"
-      // }}
+
     >
-      <PageTitleSection
-      text= "Shopping list"
-      paddingBottom= "50px"
-      />
+    
     </div>
   );
 };
