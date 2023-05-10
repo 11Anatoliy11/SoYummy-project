f40d57b841d061cf036bbd726d4bb05f1035478b
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-10 17:50:08 +0300

    complete button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,54 +14,6 @@ export const App = () => {
         backgroundColor: "#ad8686"
       }}
     >
-      Кнопочка
-      <Button
-        text="Search"
-        type ="button"
-        width ="161px"
-        height = "70px"
-        backgroundColor = "#8BAA36"
-        borderRadius = "24px 44px"
-        textColor = "#FAFAFA"
-        />
-           <Button
-        text="Search"
-        type ="button"
-        width ="161px"
-        height = "70px"
-        backgroundColor = "#22252A"
-        borderRadius = "24px 44px"
-        textColor = "#FAFAFA"
-        />
-         <Button
-        text="Other categories"
-        type ="button"
-        width ="239px"
-        height = "61px"
-        backgroundColor = "transparent"
-        textColor = "#22252A"
-        border="2px solid #8BAA36"
-        borderRadius = "24px 44px"
-        />
-          <Button
-        text="Sign in"
-        type ="button"
-        width ="141px"
-        height = "67px"
-        backgroundColor = "transparent"
-        textColor = "#FAFAFA"
-        border="2px solid #FAFAFA"
-        borderRadius = "24px 44px"
-        />
-           <Button
-        text="Subscribe"
-        type ="button"
-        width ="339px"
-        height = "60px"
-        backgroundColor = "#8BAA36"
-        textColor = "#FAFAFA"
-        borderRadius = "6px"
-        />
     </div>
   );
 };
