6f3b8aeaed1d242506e785a6eb98df76ca81ecea
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-10 15:07:46 +0300

    complete buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import Button from "./Button/Button";
+
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,58 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        backgroundColor: "#ad8686"
       }}
     >
-      React homework template
+      Кнопочка
+      <Button
+        text="Search"
+        type ="button"
+        width ="161px"
+        height = "70px"
+        backgroundColor = "#8BAA36"
+        borderRadius = "24px 44px"
+        textColor = "#FAFAFA"
+        />
+           <Button
+        text="Search"
+        type ="button"
+        width ="161px"
+        height = "70px"
+        backgroundColor = "#22252A"
+        borderRadius = "24px 44px"
+        textColor = "#FAFAFA"
+        />
+         <Button
+        text="Other categories"
+        type ="button"
+        width ="239px"
+        height = "61px"
+        backgroundColor = "transparent"
+        textColor = "#22252A"
+        border="2px solid #8BAA36"
+        borderRadius = "24px 44px"
+        />
+          <Button
+        text="Sign in"
+        type ="button"
+        width ="141px"
+        height = "67px"
+        backgroundColor = "transparent"
+        textColor = "#FAFAFA"
+        border="2px solid #FAFAFA"
+        borderRadius = "24px 44px"
+        />
+           <Button
+        text="Subscribe"
+        type ="button"
+        width ="339px"
+        height = "60px"
+        backgroundColor = "#8BAA36"
+        textColor = "#FAFAFA"
+        borderRadius = "6px"
+        />
     </div>
   );
 };
