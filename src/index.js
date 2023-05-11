775b3d4d7346daeaa86d53e88a0ddb9bef0bcac2
GROM-85 <richfron69@gmail.com>
2023-05-11 14:26:09 +0200

    redux init fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="">
+        <BrowserRouter basename="SoYummy-project">
           <App />
         </BrowserRouter>
         <Toaster position="top-center" reverseOrder={false} autoClose={2000} />
