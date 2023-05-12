6341b7c80038227af97a7a127e93f29b695de414
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-05-12 09:43:58 +0300

    rebuild pages

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter basename="SoYummy-project">
+                <BrowserRouter basename="/SoYummy-project">
                     <HelmetProvider>
                         <App />
                     </HelmetProvider>
