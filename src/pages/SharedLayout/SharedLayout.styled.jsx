649294da872fa2e2fd774d4cbe20088139c79242
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-05-13 20:28:10 +0300

    fix conteiner

--- src/pages/SharedLayout/SharedLayout.styled.jsx
+++ src/pages/SharedLayout/SharedLayout.styled.jsx
@@ -2,22 +2,20 @@ const { default: styled } = require('@emotion/styled');
 
 export const SharedLayoutContainer = styled.div`
   margin: 0 auto;
-  padding-left: 16px;
-  padding-right: 16px;
+  padding: 18px 16px 28px 16px;
+  background-color:var(--background-color);
 
   @media screen and (min-width: 375px) {
     max-width: 375px;
   }
 
   @media screen and (min-width: 768px) {
-    padding-left: 32px;
-    padding-right: 32px;
+    padding: 18px 32px 32px 32px;
     max-width: 768px;
   }
 
   @media screen and (min-width: 1280px) {
     max-width: 1280px;
-    padding-left: 20px;
-    padding-right: 20px;
+    padding: 18px 100px 50px 100px;
   }
 `;
