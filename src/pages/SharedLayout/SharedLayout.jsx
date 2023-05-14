7f40ea4d360ee9e8344b2a4735646326ce8d2eef
OlhaMal <115544382+MaksymPryshliak@users.noreply.github.com>
2023-05-15 00:00:51 +0300

    .

--- src/pages/SharedLayout/SharedLayout.jsx
+++ src/pages/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 import { Loader } from 'components/common';
-import {
-  SharedLayoutContainer
-} from './SharedLayout.styled';
+import { SharedLayoutContainer } from './SharedLayout.styled';
 import { Header } from 'components/Header/Header';
 import { Footer } from 'components/Footer/Footer';
 import Background from 'components/Background/Background';
@@ -11,13 +9,13 @@ import Background from 'components/Background/Background';
 const SharedLayout = () => {
   return (
     <Background type="Footer">
-    <SharedLayoutContainer>
-      <Header />
-      <Suspense fallback={<Loader />}>
-        <Outlet />
-      </Suspense>
-      <Footer />
-    </SharedLayoutContainer>
+      <SharedLayoutContainer>
+        <Header />
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+        <Footer />
+      </SharedLayoutContainer>
     </Background>
   );
 };
