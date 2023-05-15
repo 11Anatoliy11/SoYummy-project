4adcce12ab3f42869f2d645630d6fae6c106a056
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-15 15:43:12 +0300

    add logout logic

--- src/components/Header/User/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/Header/User/UserLogoutModal/UserLogoutModal.jsx
@@ -1,21 +1,23 @@
-// import { useDispatch } from 'react-redux';
-// import { logOut } from '../../../../redux/auth/authOperations';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../../redux/auth/auth-operations';
 
 import Modal from '../../../Modal/Modal';
 import Button from 'components/Button/Button';
 import { ButtonBox, Text } from './UserLogoutModal.styled';
 
 export const UserLogoutModal = ({ onClose }) => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const onLogout = () => {
-  //   dispatch(logOut());
-  // };
+  const onLogout = () => {
+    dispatch(logout());
+    onClose();
+  };
   return (
       <Modal className="logoutModal" onClose={onClose}>
       <Text>Are you sure you want to log out?</Text>
         <ButtonBox>
         <Button className="logoutBtn"
+        onClick={onLogout}
         type="button"
         width="137px"
         height="49px"
