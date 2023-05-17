6f957a2c415aadbcd5f83d4b0f2d58e048684f22
Mykola Pankov <pankoff97@gmail.com>
2023-05-17 12:10:55 +0300

    imp

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { store, persistor } from 'redux/store';
+import { theme } from './theme';
+import { ThemeProvider } from 'styled-components';
+
 
 import 'react-responsive-pagination/themes/minimal.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,6 +16,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
+        <ThemeProvider theme={theme}>
         <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter basename="/SoYummy-project">
                 <HelmetProvider>
@@ -20,5 +24,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                 </HelmetProvider>
             </BrowserRouter>
         </PersistGate>
+        </ThemeProvider>
     </Provider>
 );
