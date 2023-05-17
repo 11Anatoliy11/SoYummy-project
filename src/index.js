94701cae3349e169ada3965e11c55544f64611c8
Mykola Pankov <110909474+HOP3N@users.noreply.github.com>
2023-05-17 12:45:40 +0300

    Theme err fx (#52)
    * 2
    
    * +redux, src ThemeMade, components - theme.styled
    
    * imp
    
    * theme err fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { store, persistor } from 'redux/store';
-import { theme } from './theme';
+import { darkTheme } from './ThemeMade';
 import { ThemeProvider } from 'styled-components';
 
 
@@ -16,7 +16,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={darkTheme}>
         <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter basename="/SoYummy-project">
                 <HelmetProvider>
