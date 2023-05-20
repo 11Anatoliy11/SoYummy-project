bc9fdc4f47d13b8448fd6f5eb2bfd3d093234bf0
tolyakov <105534787+AnatoliyYakovenko@users.noreply.github.com>
2023-05-20 19:02:06 +0300

    Hero (#77)
    * *
    
    * create ChooseYourBreakfast component
    
    * *
    
    * complete heroTitle
    
    * add backgrounds
    
    * refactor colors for theme
    
    * *
    
    * fix button color
    
    * complete hero
    
    * delete unused
    
    * change userName color

---
+++ src/components/HeroPageTitle/HeroPageTitle.jsx
@@ -0,0 +1,16 @@
+import { Title, About } from './HeroPageTitle.styled';
+import { Accent } from 'components/ChooseYourBreakfast/ChooseYourBreakfast.styled';
+
+export const HeroPageTitle = () => {
+  return (
+    <div>
+      <Title>
+        <Accent>So</Accent>Yummy
+      </Title>
+      <About>
+        "What to cook?" is not only a recipe app, it is, in fact, your cookbook.
+        You can add your own recipes to save them for the future.{' '}
+      </About>
+    </div>
+  );
+};
