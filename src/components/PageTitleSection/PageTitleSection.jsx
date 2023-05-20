c2c171c312114512c77d5282acf25c4fb8cdd9ed
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-20 16:42:28 +0300

    delete unused

--- src/components/PageTitleSection/PageTitleSection.jsx
+++ src/components/PageTitleSection/PageTitleSection.jsx
@@ -1,6 +1,5 @@
 import { TitleContainer, Title } from './PageTitleSection.styled';
 import { ReactComponent as Rectangle } from 'images/svg/rectangle.svg';
-import { ReactComponent as Sphere } from 'images/svg/round.svg';
 
 export default function PageTitleSection({ text }) {
   return (
