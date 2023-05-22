365a732983b9dc8fffa71fd66f3386e41c256ef2
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-22 21:42:18 +0300

    complete preview without location fix

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,33 +1,36 @@
-import{ useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
-import {RecipeCard} from 'components/Common/recipeCard/RecipeCard';
-import{recipeSelector} from 'redux/recipes/recipe-select'
-import{recipeMainPage} from 'redux/recipes/recipe-operation'
+import { RecipeCard } from 'components/Common/recipeCard/RecipeCard';
+import { recipeSelector } from 'redux/recipes/recipe-select';
+import { recipeMainPage } from 'redux/recipes/recipe-operation';
 import { useIsMobileScreen } from 'hooks/useIsMobileScreen';
 import { useIsSmallScreen } from 'hooks/useIsSmallScreen';
 
-
-// import { useLocation, useParams } from "react-router-dom";
-
-
-import {Wrapper, MainList, CategoryWrapper, CategoryList, CategoryTitle, Button, OtherCategoryBtn } from "./PreviewCategories.styled";
-
+import {
+  Wrapper,
+  MainList,
+  CategoryWrapper,
+  CategoryList,
+  CategoryTitle,
+  Button,
+  OtherCategoryBtn,
+} from './PreviewCategories.styled';
 
 export const PreviewCategories = () => {
   const isMobile = useIsMobileScreen();
   const isTablet = useIsSmallScreen();
 
-  const recipesByMediaHandle = (recipes) => {
+  const recipesByMediaHandle = recipes => {
     if (isMobile) {
-        return recipes.slice(0, 1);
+      return recipes.slice(0, 1);
     }
     if (isTablet) {
-        return recipes.slice(0, 2);
+      return recipes.slice(0, 2);
     }
     return recipes;
-};
+  };
 
   const dispatch = useDispatch();
 
@@ -35,37 +38,39 @@ export const PreviewCategories = () => {
     dispatch(recipeMainPage());
   }, [dispatch]);
 
-const recipes = useSelector(recipeSelector.getRecipeMainPage);
+  const recipes = useSelector(recipeSelector.getRecipeMainPage);
 
-return (
-  <Wrapper>
-    {recipes.length>0 && (
-      <MainList key={nanoid()}>
-        {recipes?.map(({ category, documents }) => {
-          return (
-            <CategoryWrapper key={category}>
-              <CategoryTitle>{category}</CategoryTitle>
-              <CategoryList>
-              {recipesByMediaHandle(documents)?.map(({ _id, thumb, title }) => {
-                return (
-                  <RecipeCard
-                    key={_id}
-                    thumb={thumb}
-                    title={title}
-                    id={_id}
-                  />
-                );
-              })}
+  return (
+    <Wrapper>
+      {recipes.length > 0 && (
+        <MainList key={nanoid()}>
+          {recipes?.map(({ category, documents }) => {
+            return (
+              <CategoryWrapper key={category}>
+                <CategoryTitle>{category}</CategoryTitle>
+                <CategoryList>
+                  {recipesByMediaHandle(documents)?.map(
+                    ({ _id, thumb, title }) => {
+                      return (
+                        <RecipeCard
+                          key={_id}
+                          thumb={thumb}
+                          title={title}
+                          id={_id}
+                        />
+                      );
+                    }
+                  )}
                 </CategoryList>
-              <Button to={`/categories/${category}`}>See all</Button>
+                <Button to={`/categories/${category}`}>See all</Button>
               </CategoryWrapper>
-          );
-        })}
-      </MainList>
-    )}
-       <OtherCategoryBtn to="/categories/Beef">
-         Other categories
-       </OtherCategoryBtn>
-  </Wrapper>
-);
+            );
+          })}
+        </MainList>
+      )}
+      <OtherCategoryBtn to="/categories/Beef">
+        Other categories
+      </OtherCategoryBtn>
+    </Wrapper>
+  );
 };
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,12 @@
-
-
 import { Main } from 'components/Main/Main';
 import { PreviewCategories } from 'components/PreviewCategories';
 
-
-
 const MainPage = () => {
-
-  return(
+  return (
     <>
-    <Main/>
-    <PreviewCategories/>
+      <Main />
+      <PreviewCategories />
     </>
-
   );
 };
 
