cd8beab5cb4399ddc125ab6c4d041b5572e65691
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-22 15:35:01 +0300

    work with mui modal

--- src/components/Header/User/UserLogoModal/UserLogoModal.jsx
+++ src/components/Header/User/UserLogoModal/UserLogoModal.jsx
@@ -3,32 +3,60 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Button from 'components/Button/Button';
 import Modal from 'components/Modal/Modal';
 
+import Typography from '@mui/material/Typography';
+import Popover from '@mui/material/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+
 import { EditWrapper, EditText, ModalWrapper } from './UserLogoModal.styled';
+
+
 
 export const UserLogoModal = ({ onClose, onLogout, onEdit }) => {
   return (
-    <Modal
-      className="userLogoModal"
-      overlayClassName="userLogoOverlayModal"
-      onClose={onClose}
-    >
-      <ModalWrapper>
-        <EditWrapper onClick={onEdit}>
-          <EditText>Edit profile</EditText>
-          <Edit />
-        </EditWrapper>
-        <Button
-          className="btn"
-          type="button"
-          width="125px"
-          height="43px"
-          border="none"
-          borderRadius="24px 44px"
-          onClick={onLogout}
-        >
-          Log out <ArrowForwardIcon />
+    <PopupState variant="popover" popupId="demo-popup-popover">
+    {(popupState) => (
+      <div>
+        <Button variant="contained" {...bindTrigger(popupState)}>
+          Open Popover
         </Button>
-      </ModalWrapper>
-    </Modal>
+        <Popover
+          {...bindPopover(popupState)}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+        </Popover>
+      </div>
+    )}
+  </PopupState>
   );
 };
+{/* <Modal
+className="userLogoModal"
+overlayClassName="userLogoOverlayModal"
+onClose={onClose}
+>
+<ModalWrapper>
+  <EditWrapper onClick={onEdit}>
+    <EditText>Edit profile</EditText>
+    <Edit />
+  </EditWrapper>
+  <Button
+    className="btn"
+    type="button"
+    width="125px"
+    height="43px"
+    border="none"
+    borderRadius="24px 44px"
+    onClick={onLogout}
+  >
+    Log out <ArrowForwardIcon />
+  </Button>
+</ModalWrapper>
+</Modal> */}
