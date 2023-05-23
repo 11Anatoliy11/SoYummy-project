216036714ac5a7e5dee31cfe512681a6f6e2f938
tolyakov <105534787+AnatoliyYakovenko@users.noreply.github.com>
2023-05-23 12:41:45 +0300

    User modals. fix position problem (#100)
    * work with modalIcon
    
    * styles of modals
    
    * work with UserLogoModal
    
    * add modal styles
    
    * complete UserLogoutModal styles
    
    * complete modals styles
    
    * change structure
    
    * ability to add person image
    
    * fix problem with closeButton
    
    * change position in UserLogoModal
    
    * add logout logic
    
    * fix some issues
    
    * test
    
    * change icon
    
    * add some changes
    
    * refactor in UserInfoModal
    
    * *
    
    * fix problems with submit
    
    * add container for avatar
    
    * add url check for avatar
    
    * complete refactoring for dark theme
    
    * work with mui modal
    
    * style of mui
    
    * change custom modal of LogoModal onto mui element

--- src/components/Header/User/UserLogoModal/UserLogoModal.jsx
+++ src/components/Header/User/UserLogoModal/UserLogoModal.jsx
@@ -1,34 +1,59 @@
 import { ReactComponent as Edit } from 'images/svg/edit.svg';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Button from 'components/Button/Button';
-import Modal from 'components/Modal/Modal';
 
-import { EditWrapper, EditText, ModalWrapper } from './UserLogoModal.styled';
+import Typography from '@mui/material/Typography';
 
-export const UserLogoModal = ({ onClose, onLogout, onEdit }) => {
+import {
+  EditWrapper,
+  EditText,
+  ModalWrapper,
+  PopoverWrapper,
+  PopoverStyled,
+} from './UserLogoModal.styled';
+
+export const UserLogoModal = ({
+  onClose,
+  onLogout,
+  onEdit,
+  anchorEl,
+  open,
+  id,
+}) => {
   return (
-    <Modal
-      className="userLogoModal"
-      overlayClassName="userLogoOverlayModal"
-      onClose={onClose}
-    >
-      <ModalWrapper>
-        <EditWrapper onClick={onEdit}>
-          <EditText>Edit profile</EditText>
-          <Edit />
-        </EditWrapper>
-        <Button
-          className="btn"
-          type="button"
-          width="125px"
-          height="43px"
-          border="none"
-          borderRadius="24px 44px"
-          onClick={onLogout}
-        >
-          Log out <ArrowForwardIcon />
-        </Button>
-      </ModalWrapper>
-    </Modal>
+    <PopoverWrapper>
+      <PopoverStyled
+        anchorReference="buttonPosition"
+        anchorPosition={{ top: 20, left: 30 }}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={onClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 0 }}>
+          <ModalWrapper>
+            <EditWrapper onClick={onEdit}>
+              <EditText>Edit profile</EditText>
+              <Edit />
+            </EditWrapper>
+            <Button
+              className="btn"
+              type="button"
+              width="125px"
+              height="43px"
+              border="none"
+              borderRadius="24px 44px"
+              onClick={onLogout}
+            >
+              Log out <ArrowForwardIcon />
+            </Button>
+          </ModalWrapper>
+        </Typography>
+      </PopoverStyled>
+    </PopoverWrapper>
   );
 };
