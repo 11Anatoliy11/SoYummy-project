523b29acbbfc7c7ad04560b79b8666ac94af5f80
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-05-25 12:18:47 +0300

    fix bugs

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,11 @@
-// import PageTitleSection from "components/PageTitleSection/PageTitleSection";
+import PageTitleSection from "components/PageTitleSection/PageTitleSection";
 import { ReactComponent as NotFoundIcon } from 'images/svg/404.svg';
 import { NotFoundContainer, NotFoundWrapper , SorryTitle, ExplanText } from './NotFound.styled';
 
 export const NotFound = () => {
   return (
     <NotFoundContainer>
-      {/* <PageTitleSection text={""}/> */}
+      <PageTitleSection text={""}/>
       <NotFoundWrapper>
         <NotFoundIcon className='icon'/>
         <SorryTitle>We are sorry,</SorryTitle>
